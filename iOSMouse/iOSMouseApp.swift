cc8580fc80debcd617d8d31a78473d39b0d619bf
Imran Razak <77019152+ImranRazak1@users.noreply.github.com>
2024-05-06 18:29:33 +0100

    Initial Commit

---
+++ iOSMouse/iOSMouseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSMouseApp.swift
+//  iOSMouse
+//
+//  Created by Imran razak on 06/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct iOSMouseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
